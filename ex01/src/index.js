c400d8c3ed86f537efdf5ad2ba91f8c73d41369c
Alma <96235009+alma1331@users.noreply.github.com>
2022-03-31 09:38:21 +0200

    Add files via upload

---
+++ ex01/src/index.js
@@ -0,0 +1,20 @@
+function main(numOne, numTwo) {
+
+
+var quotient;
+var numOne;
+var numTwo;
+var quotient = numOne / numTwo;
+if(numTwo === 0) {
+quotient = 'Divide by 0! Cannot divide by zero!'
+} else {
+
+
+
+}
+ 
+  return quotient;
+}
+
+console.log(main(15, 3), main(25, 5), main(420, 10), main(55, 0));
+module.exports = main;

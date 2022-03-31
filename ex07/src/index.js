35e4f159e1be5000b872e46f8dab75eae4148c89
Alma <96235009+alma1331@users.noreply.github.com>
2022-03-31 15:26:20 +0200

    Add files via upload

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+ var  remainder;
+ var  numOne;
+ var  numTwo;
+ var  remainder = numOne % numTwo;
+
+return remainder;
+}
+
+console.log(main(5, 2), main(15, 4), main(102, 25));
+module.exports = main;

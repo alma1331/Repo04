91d6ec93dcab84f98e5cc52ea2de622dfa4ce7a2
Alma <96235009+alma1331@users.noreply.github.com>
2022-03-31 15:12:12 +0200

    Add files via upload

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+ var  product;
+ var  numOne;
+ var  numTwo;
+ var  product = numOne *  numTwo;
+
+return product;
+}
+
+console.log(main(2.5, 2.0), main(5.5, 5.5), main(100.25, 43.20));
+module.exports = main;
